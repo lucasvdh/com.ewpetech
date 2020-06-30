0cc298a2caee19e7c8c8bcea94ef76a9ff9f32e9
Lucas van der Have <lucasvdhave@gmail.com>
2020-06-30 21:19:55 +0200

    feat: initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict"
+
+
+const {encrypt, decrypt, defaultKey} = require('./lib/encryptor');
+
+
+console.log(decrypt('E7tvuPbwppr8O5alYYBgi7LwZ/bmg4f8hP8LyhjNxp9hLHC32TI9KCwvcoe3Lzrp2Nz/AqGRMwbYg2D3s54FKA=='));
+console.log(decrypt('T2tGu9JTsZPLMhoPO/mBclFGrb4nkisXz6usYH5VUzRzkEQfdDwGvD7dLOO0SGBpOst6YnVi0dH1uC30cssBWYzD1k6jjxiwE2rMrTg7Xnk='));
